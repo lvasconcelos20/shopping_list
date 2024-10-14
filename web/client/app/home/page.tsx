d330e1c2ec22df0f2f4a81eb10c3834d13bc16fc
Felipe Vasconcelos <lipevasconcelos20@gmail.com>
2024-10-14 15:51:36 -0300

    dependencies

--- web/client/app/home/page.tsx
+++ web/client/app/home/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
     return <div className="flex justify-center items-center w-full h-full">
-         <Button className="bg-slate-600">
-         Button
-        </Button>
+        
 
     </div>
     
