db306b6e1010e286957f09611825fc7a1c26c347
Felipe Vasconcelos <lipevasconcelos20@gmail.com>
2024-10-17 17:21:33 -0300

    installation of dependencies

--- web/client/app/home/index.tsx
+++ web/client/app/home/index.tsx
@@ -4,6 +4,8 @@ import { Background } from '../assets'
 import { Label } from '@radix-ui/react-label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import Checkbox from '@mui/material/Checkbox';
+
 import {
   Select,
   SelectContent,
@@ -12,20 +14,18 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { Checkbox } from '@radix-ui/react-checkbox';
+
 
 
 export default function Home() {
-
   const items = [
     { name: 'Maçã', quantity: '2 unidades', category: 'fruta', icon: '🍎', color: 'bg-red-500' },
     { name: 'Pão francês', quantity: '4 unidades', category: 'padaria', icon: '🥖', color: 'bg-yellow-500' },
   ];
 
   return (
-    <div  className="bg-[#0C0C0D] font-inter flex justify-center items-center">
+    <div className="bg-[#0C0C0D] font-inter flex justify-center items-center">
       <header className="relative w-full h-screen font-inter">
-        {/* Imagem de fundo */}
         <div className="relative w-full h-[185px]">
           <Image 
             src={Background} 
@@ -37,105 +37,87 @@ export default function Home() {
           </h2>
         </div>     
       </header>
-      <div className="absolute left-[360px] top-[180px] flex justify-center mt-[-18px]">
-          <form className="flex gap-11"> 
-            <div className="relative w-[326px]">
-              <Input 
-                id="item-input" 
-                type="text" 
-                className="w-full h-[40px] border rounded-md placeholder-transparent  text-gray-400"
-                style={{border: '1px solid rgba(37, 37, 41, 1)', backgroundColor: 'rgba(17, 17, 18, 1)'}}
-              />
+      <div className="absolute left-[360px] top-[180px] mt-[-20px] w-full max-w-[600px]">
+        {/* Formulário */}
+        <form className="flex gap-12 items-end">
+          <div className="relative w-[326px]">
+            <Input 
+              id="item-input" 
+              type="text" 
+              className="w-full h-[40px] border rounded-md text-gray-400"
+              style={{ border: '1px solid rgba(37, 37, 41, 1)', backgroundColor: 'rgba(17, 17, 18, 1)' }}
+            />
+            <Label 
+              htmlFor="item-input" 
+              className="absolute left-2 top-[-20px] text-sm text-gray-400"
+            >
+              Item
+            </Label>
+          </div>
+          <div className="relative w-[100px]">
+            <Select>
+              <SelectTrigger id="quantidade-select" className="peer w-full h-[40px] text-gray-400" style={{ border: '1px solid rgba(37, 37, 41, 1)', background: 'rgba(23, 23, 26, 1)' }}>
+                <SelectValue placeholder="Selecione" />
+              </SelectTrigger>
               <Label 
-                htmlFor="item-input" 
+                htmlFor="quantidade-select" 
                 className="absolute left-2 top-[-20px] text-sm text-gray-400"
               >
-                Item
+                Quantidade
               </Label>
-            </div>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="2">2</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="relative w-[150px]">
+            <Select>
+              <SelectTrigger id="categoria-select" className="peer w-full h-[40px] text-gray-400" style={{ border: '1px solid rgba(37, 37, 41, 1)', background: 'rgba(23, 23, 26, 1)' }}>
+                <SelectValue placeholder="Selecione" />
+              </SelectTrigger>
+              <Label 
+                htmlFor="categoria-select" 
+                className="absolute left-2 top-[-20px] text-sm text-gray-400"
+              >
+                Categoria
+              </Label>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="alimento">Alimento</SelectItem>
+                  <SelectItem value="limpeza">Limpeza</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <Button className="text-white rounded-full w-[40px] h-[40px] flex items-center justify-center" style={{ background: 'rgba(116, 80, 172, 1)', fontSize: '35px' }}>
+            +
+          </Button>
+        </form>
 
-            {/* Quantidade */}
-            <div className="relative w-[100px]">
-              <Select>
-                <SelectTrigger id="quantidade-select" className="peer w-full h-[40px] text-gray-400" style={{border: '1px solid rgba(37, 37, 41, 1)', background: 'rgba(23, 23, 26, 1)'}}>
-                  <SelectValue placeholder="Selecione" />
-                </SelectTrigger>
-                <Label 
-                  htmlFor="quantidade-select" 
-                  className="absolute left-2 top-[-20px] text-sm text-gray-400"
-                >
-                  Quantidade
-                </Label>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="1">1</SelectItem>
-                    <SelectItem value="2">2</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-
-            {/* Categoria */}
-            <div className="relative w-[150px]">
-              <Select>
-                <SelectTrigger id="categoria-select" className="peer w-full h-[40px] text-gray-400" style={{border: '1px solid rgba(37, 37, 41, 1)', background: 'rgba(23, 23, 26, 1)'}}>
-                  <SelectValue placeholder="Selecione" />
-                </SelectTrigger>
-                <Label 
-                  htmlFor="categoria-select" 
-                  className="absolute left-2 top-[-20px] text-sm text-gray-400"
-                >
-                  Categoria
-                </Label>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="alimento">Alimento</SelectItem>
-                    <SelectItem value="limpeza">Limpeza</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            {/* Botão de adicionar */}
-            <Button className="text-white rounded-full w-[40px] h-[40px] flex items-center justify-center" style={{background: 'rgba(116, 80, 172, 1)', fontSize: '35px'}}>
-              +
-            </Button>
-            <ul className="flex flex-col gap-12">
+        {/* Lista de Itens */}
+        <ul className="mt-30 flex flex-col gap-15">
           {items.map((item, index) => (
             <li
               key={index}
               className="flex items-center justify-between bg-[#171717] h-[60px] rounded-lg shadow p-4"
             >
               <div className="flex items-center gap-4">
-                <Checkbox
-                 
-
-                  className="w-4 h-4 bg-white border border-gray-400 rounded-sm checked:bg-purple-600"
-                />
+                <Checkbox style={{ color: 'rgba(168, 129, 230, 1)' }} />
                 <div>
-                  <h3 className={`text-white font-bold  'line-through text-gray-500' : ''}`}>
-                    {item.name}
-                  </h3>
-                  <p className={`text-xs text-gray-400 'line-through text-gray-500' : ''}`}>
-                    {item.quantity}
-                  </p>
+                  <h3 className="text-white font-bold">{item.name}</h3>
+                  <p className="text-xs text-gray-400">{item.quantity}</p>
                 </div>
               </div>
-
-              {/* Parte direita: ícone */}
-              <div className="flex items-center gap-11">
-                <span className={`text-lg ${item.color} p-2 rounded-full`}>
-                  {item.icon}
-                </span>
-              </div>
+              <span className={`text-lg ${item.color} p-2 rounded-full`}>
+                {item.icon}
+              </span>
             </li>
           ))}
         </ul>
-          </form>
-        </div>  
+      </div>
     </div>
-    
-    
   )
 }
-
-
